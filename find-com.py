99c58a77822cd0d58ac978da1aa5dd802f342214
Zorry Belcheva <belcheva@quasar.strw.leidenuniv.nl>
2019-06-18 15:45:20 +0200

    Recent changes; make all funcs

--- find-com.py
+++ find-com.py
@@ -1,20 +1,9 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import h5py
-import time
-import hydrangea_tools as ht
 import sim_tools as st
 import yb_utils as yb
+import hydrangea_tools as ht
 from astropy.io import ascii
-
-
-def bootstrap(sample):
-    size = len(sample)
-    boot = np.zeros(size)
-    for i in range(size):
-        rand = np.random.randint(0, size-1)
-        boot[i] = sample[rand]
-    return boot
 
 
 def gaussian(x, mu, sigma):
@@ -29,21 +18,21 @@ def weight_function(origin, position, R):
     return gaussian(d, 0, R)
 
 
-def find_CoM(origin, pos, mass, R, ind, ptype):
+def find_CoM(origin, pos, mass, R, ptype):
     numer = np.array((0, 0, 0))
     denom = np.array((0, 0, 0))
 
     wmax = weight_function(origin, origin, R)
-    # print(wmax)
+    print(wmax)
 
     if ptype == 1:
         m_dm = mass[0]
-        for i in range(len(ind)):
+        for i in range(len(pos)):
             numer = numer + np.array(pos[i])*m_dm*weight_function(origin, pos[i], R)
             denom = denom + m_dm * weight_function(origin, pos[i], R)
             # print('{:1.6f}'.format(weight_function(origin, pos[i], R) / wmax))
     else:
-        for i in range(len(ind)):
+        for i in range(len(pos)):
             numer = numer + np.array(pos[i])*mass[i]*weight_function(origin, pos[i], R)
             denom = denom + mass[i]*weight_function(origin, pos[i], R)
             # print(weight_function(origin, pos[i], R)/wmax)
@@ -51,35 +40,43 @@ def find_CoM(origin, pos, mass, R, ind, ptype):
     return numer/denom
 
 
-def bootstrap_CoM(galID, ptype, size):
-    start = time.time()
+def CoM_auto(galID, ptype):
+    # ----------------------------------------
 
-    # --------------------- PRELIMINARIES -----------------------------------
     cluster_index = 29
-    plot_snap = 29
+    plot_snap = 29  # Which snapshot to plot
+    imsize = 0.01  # (Half-)image size in pMpc
+
+    # ---------------------------------------
+
     rundir = '/net/quasar/data3/Hydrangea/CE-' + str(cluster_index) + '/HYDRO/'
-    snapdir = st.form_files(rundir, isnap=plot_snap, types='snap')
+
     posloc = rundir + '/highlev/GalaxyPositionsSnap.hdf5'
+    spiderloc = rundir + '/highlev/SpiderwebTables.hdf5'
     fgtloc = rundir + '/highlev/FullGalaxyTables.hdf5'
 
     snaplistloc = rundir + "sneplists/allsnaps.dat"
     snaplist = ascii.read(snaplistloc)
     aexpSnap = np.array(snaplist['aexp'])
 
+    # Shortcut of loading the positions of all galaxies
+    pos_allGal = yb.read_hdf5(posloc, "Centre")[:, plot_snap, :]
+    pos_gal = pos_allGal[galID, :]
+
     # Slightly lazy way to set comoving <-> proper conversion factor
     aexp_factor = aexpSnap[plot_snap]
     h_factor = 1 / 0.6777
     conv_astro_pos = h_factor * aexp_factor
 
-    # ----------------------- SPECIFICS -------------------------------------
-    imsize = 0.01   # half image size in pMpc
-    R = h5py.File(fgtloc, 'r')['StellarHalfMassRad']
-    R = R[galID, plot_snap]  # stellar half-mass radius of 2373, pMpc
+    print("Determined galaxy position as {:.3f}/{:.3f}/{:.3f}..."
+          .format(*pos_gal))
 
-    pos_allGal = yb.read_hdf5(posloc, "Centre")[:, plot_snap, :]
-    pos_gal = pos_allGal[galID, :]
+    # snapdir = st.form_files(rundir, isnap=plot_snap, types='sub')
+    snapdir = st.form_files(rundir, isnap=plot_snap, types='snap')
 
     readReg = ht.ReadRegion(snapdir, ptype, [*pos_gal / conv_astro_pos, imsize * np.sqrt(3) / conv_astro_pos])
+
+    # Read position and mass of particles in target region
     pos = readReg.read_data("Coordinates", astro=True)
 
     if ptype != 1:
@@ -89,41 +86,26 @@ def bootstrap_CoM(galID, ptype, size):
         mass = np.zeros(pos.shape[0]) + st.m_dm(snapdir, issnapdir=True, astro=True)
         ind = np.loadtxt('output/indexing_array_'+str(galID)+'.txt', unpack=True).astype(int)
 
+    # print(pos[ind])
+    # print(mass[ind])
+
     pos = pos[ind]
     mass = mass[ind]
-    dist = np.zeros(size)
-    dx = np.zeros(size)
-    dy = np.zeros(size)
-    dz = np.zeros(size)
 
-    f = open('output/displacement_'+str(ptype)+'_'+str(galID)+'.txt', 'a')
+    R = h5py.File(fgtloc, 'r')['StellarHalfMassRad']
+    R = R[galID, plot_snap]  # stellar half-mass radius of 2373, pMpc
 
-    for i in range(size):
-        index = np.arange(len(pos))
-        index = bootstrap(index).astype(int)
+    CoM = find_CoM(pos_gal, pos, mass, R, ptype)
+    print('\n')
+    print('CoM \t', CoM)
+    print('origin \t', pos_gal)
+    print('\n')
 
-        CoM = find_CoM(pos_gal, pos[index], mass[index], R, ind, ptype)
-        # print('\n')
-        # print('CoM \t', CoM)
-        # print('origin \t', pos_gal)
-        # print('\n')
+    d = abs(pos_gal - CoM)
+    displacement = np.sqrt(sum(d**2))
 
-        d = abs(pos_gal - CoM)
-        dist[i] = np.sqrt(sum(d ** 2))
-        dx[i] = CoM[0] - pos_gal[0]
-        dy[i] = CoM[1] - pos_gal[1]
-        dz[i] = CoM[2] - pos_gal[2]
-
-        f.write('{}\t{}\t{}\t{}\n'.format(dx[i], dy[i], dz[i], dist[i]))
-
-    f.close()
-
-    print(np.average(dist)*1000)
-    print(np.std(dist)*1000)
-
-    end = time.time()
-
-    print('\nElapsed: ', end-start, '\n')
+    print('CoM displacement: [pkpc]\t', displacement*1e6)
+    print('\n')
 
 
-bootstrap_CoM(galID=4452, ptype=1, size=1000)
+CoM_auto(galID=4452, ptype=4)
